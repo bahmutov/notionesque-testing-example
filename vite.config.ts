c005219a182ca5f921fe2df3fc3956e94f794a7d
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2025-03-17 16:36:38 -0400

    instrument the app

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,25 @@
 import { defineConfig } from "vitest/config"
 import react from "@vitejs/plugin-react"
-import tailwindcss from '@tailwindcss/vite'
-
+import tailwindcss from "@tailwindcss/vite"
+// https://github.com/iFaxity/vite-plugin-istanbul
+import IstanbulPlugin from "vite-plugin-istanbul"
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
+    IstanbulPlugin({
+      include: "src/*",
+      exclude: ["node_modules", "cypress/", "*.cy.js"],
+      extension: [".js", ".jsx", ".ts", ".tsx"],
+    }),
   ],
+  build: {
+    sourcemap: true,
+  },
   server: {
-    open: true,
+    open: false,
   },
   test: {
     globals: true,
