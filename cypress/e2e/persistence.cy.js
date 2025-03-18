5c0fbc59aa61ddc8672329d30d47146d1f0910aa
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2025-03-18 09:31:35 -0400

    test persistence using cy-spok

--- cypress/e2e/persistence.cy.js
+++ cypress/e2e/persistence.cy.js
@@ -1,3 +1,5 @@
+import spok from "cy-spok"
+
 describe("App persistence", () => {
   it("persists the tasks", () => {
     cy.step("Create 2 tasks")
@@ -46,5 +48,57 @@ describe("App persistence", () => {
         })
         expect(items[1], "task 2 id").to.have.property("id").to.match(uuidRegex)
       })
+  })
+
+  it("persists the tasks (spok version)", () => {
+    cy.step("Create 2 tasks")
+    // control the clock so we know the timestamps
+    const timestamp = "2023-01-01T00:00:00.000Z"
+    const started = new Date(timestamp)
+    cy.clock(started, ["Date"])
+    cy.home()
+    cy.addTask("Write code", "Write code description")
+    cy.addTask("Write tests", "Write tests description")
+
+    const uuidRegex =
+      /^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$/
+
+    cy.step("Check the local storage")
+    cy.window()
+      .its("localStorage")
+      .invoke("getItem", "persist:tasks")
+      .apply(JSON.parse)
+      .should(
+        spok({
+          present: spok.string,
+        }),
+      )
+      .its("present")
+      .apply(JSON.parse)
+      .its("items")
+      .should(
+        spok([
+          {
+            createdAt: timestamp,
+            customFields: {},
+            description: "Write code description",
+            priority: "none",
+            status: "not started",
+            title: "Write code",
+            updatedAt: timestamp,
+            id: spok.test(uuidRegex),
+          },
+          {
+            createdAt: timestamp,
+            customFields: {},
+            description: "Write tests description",
+            priority: "none",
+            status: "not started",
+            title: "Write tests",
+            updatedAt: timestamp,
+            id: spok.test(uuidRegex),
+          },
+        ]),
+      )
   })
 })
