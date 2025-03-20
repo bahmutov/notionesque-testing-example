2967456c0700239866013d1d68770f4e9e284e6a
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2025-03-20 14:55:11 -0400

    add more jsdoc

--- cypress/support/index.d.ts
+++ cypress/support/index.d.ts
@@ -10,6 +10,14 @@ declare namespace Cypress {
      * @param zeroTasks - If true, check for zero tasks.
      */
     home(zeroTasks?: boolean): Chainable<void>
+
+    /**
+     * Add a single task.
+     * @param title - The title of the task.
+     * @param description - The description of the task.
+     * @param priority - The priority of the task (optional).
+     * @param status - The status of the task (optional).
+     */
     addTask(
       title: string,
       description: string,
@@ -17,6 +25,11 @@ declare namespace Cypress {
       status?: status,
     ): Chainable<void>
 
+    /**
+     * Add multiple tasks.
+     * @param tasks - An array of task objects. Each object should at least have
+     * a `title` and a `description` property and `description` strings.
+     */
     addTasks(tasks: unknown[]): Chainable<void>
   }
 }
