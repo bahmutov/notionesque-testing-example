26cb0b1852a1eebb83e74fe682ceac3d11f984f6
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2025-03-20 16:05:07 -0400

    hmr comment

--- vite.config.ts
+++ vite.config.ts
@@ -20,6 +20,7 @@ export default defineConfig({
   },
   server: {
     open: false,
+    // no need to hot reload in CI
     hmr: process.env.CI ? false : true,
     watch: {
       ignored: ["node_modules", "cypress", "*.cy.js", "coverage"],
